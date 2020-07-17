cc99acc98438de501524234e51f5cd68e317290c
Charlie Hu <chenleihu@google.com>
2020-07-17 12:34:54 -0400

    :fire: remove commented code

--- server.mjs
+++ server.mjs
@@ -52,7 +52,6 @@ app.use(function commonHeaders(req, res, next) {
 
 app.get('/test', (req, res, next) => {
   const on = 'on' in req.query;
-  // res.append('Feature-Policy', `geolocation 'self' https://example.com`);
   if (on) {
     res.append('Feature-Policy', `max-downscaling-image 'none'`);
     res.append('Feature-Policy', `image-compression 'none'`);
@@ -72,29 +71,8 @@ app.get('/test', (req, res, next) => {
 });
 
 app.get('/:demoPage', (req, res, next) => {
-  // const demoPage = req.params.demoPage;
-  // console.log(demoPage)
   res.send(fs.readFileSync('./public/index.html', {encoding: 'utf-8'}));
 });
-
-// // Enable/disable policies on demo pages.
-// app.use('/demos', (req, res, next) => {
-//   const unsizedMedia = 'on' in req.query;
-//   // res.append('Feature-Policy', "camera 'none'; microphone 'none'");
-//   // res.append('Feature-Policy', "autoplay 'self' https://clips.vorwaerts-gmbh.de");
-//   // res.append('Feature-Policy', "fullscreen 'none'");
-//   // res.append('Feature-Policy', "geolocation 'self' https://example.com");
-//   // res.append('Feature-Policy', "midi 'none'");
-//   // res.append('Feature-Policy', "sync-xhr 'none'");
-//   // res.append('Feature-Policy', "vr 'none'");
-//   // res.append('Feature-Policy', "usb 'none'");
-//   // res.append('Feature-Policy', "payment 'none'");
-//   // res.append('Feature-Policy', "vibrate 'none'");
-//   if (unsizedMedia) {
-//     // res.set('Feature-Policy', `unsized-media 'none'`);
-//   }
-//   next();
-// });
 
 app.use(express.static('public', {extensions: ['html', 'htm']}));
 app.use(express.static('node_modules'));
